48d99588f82ca040f1b49ff58a4ff6482e203926
Yusef-Mohamed <yoousefmohaamd@gmail.com>
2023-08-16 18:54:04 +0300

    0

--- app/actions/getCurrentUser.ts
+++ app/actions/getCurrentUser.ts
@@ -2,8 +2,7 @@ import { getServerSession } from "next-auth";
 import { authOptions } from "../api/auth/[...nextauth]/route";
 import prisma from "@/app/libs/prismadb";
 export async function getSession() {
-  return;
-  // await getServerSession(authOptions)
+  return await getServerSession(authOptions);
 }
 export default async function getCurrentUser() {
   try {
